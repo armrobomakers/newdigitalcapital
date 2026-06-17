0ae9a33b52376b840e8bc984abe89bcce47c16a7
AlexandrRyzhkov92 <ryzhkovinvest@gmail.com>
2026-06-17 17:28:07 +0500

    balance footer cta placement

--- components/landing.tsx
+++ components/landing.tsx
@@ -758,7 +758,7 @@ export function LandingPage() {
                 <p className="mt-3 text-sm leading-7 text-white/66">{eventData.footer.ctaCopy}</p>
               </div>
 
-              <div className="flex flex-col items-start gap-4 lg:absolute lg:left-[73%] lg:top-[73%] lg:z-20 lg:flex-row lg:-translate-x-1/2 lg:-translate-y-1/2 lg:items-center">
+              <div className="flex flex-col items-start gap-4 lg:absolute lg:left-[68%] lg:top-[81%] lg:z-20 lg:flex-row lg:-translate-x-1/2 lg:-translate-y-1/2 lg:items-center">
                 <Link href="#register" className="btn-primary min-w-[286px]">
                   {eventData.footer.ctaButton}
                 </Link>
